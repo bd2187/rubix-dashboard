8b5ad7b9c13df3e7392242067b597b98e49b48b2
bd2187 <brandon.dionisio.26@gmail.com>
2019-07-05 16:11:00 -0700

    Initial commit. Create sample friends array

---
+++ src/config/sample-data.js
@@ -0,0 +1,86 @@
+export const friends = [
+    {
+        name: "jordyn-ouellet",
+        display_name: "Jordyn Ouellet",
+        status: "online",
+        id: "001"
+    },
+    {
+        name: "ava-parry",
+        display_name: "Ava Parry",
+        status: "online",
+        id: "002"
+    },
+    {
+        name: "angelina-mills",
+        display_name: "Angelina Mills",
+        status: "online",
+        id: "003"
+    },
+    {
+        name: "crystal-ford",
+        display_name: "Crystal Ford",
+        status: "online",
+        id: "004"
+    },
+    {
+        name: "toby-king",
+        display_name: "Toby king",
+        status: "idle",
+        id: "005"
+    },
+    {
+        name: "ju-lan",
+        display_name: "Ju Lan",
+        status: "idle",
+        id: "006"
+    },
+    {
+        name: "lana-collin",
+        display_name: "Lana Collin",
+        status: "idle",
+        id: "007"
+    },
+    {
+        name: "alexandra-mordin",
+        display_name: "Alenda Mordin",
+        status: "idle",
+        id: "008"
+    },
+    {
+        name: "jonas-schafer",
+        display_name: "Jonas Schafer",
+        status: "busy",
+        id: "009"
+    },
+    {
+        name: "ricardo-ibarra",
+        display_name: "Ricardo Ibarra",
+        status: "busy",
+        id: "010"
+    },
+    {
+        name: "the-unknown",
+        display_name: "The Unknown",
+        status: "busy",
+        id: "011"
+    },
+    {
+        name: "evan-poulain",
+        display_name: "Evan Poulain",
+        status: "offline",
+        id: "012"
+    },
+    {
+        name: "canan-erdem",
+        display_name: "Canan Erdem",
+        status: "offline",
+        id: "013"
+    },
+    {
+        name: "antelope-inc",
+        display_name: "Antelope Inc.",
+        status: "offline",
+        id: "014"
+    }
+];
