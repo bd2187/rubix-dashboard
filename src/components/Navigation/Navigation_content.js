01448cd80a87e2a1a791c88ea92e3c866276c84c
bd2187 <brandon.dionisio.26@gmail.com>
2019-07-05 09:18:58 -0700

    Import panel components

--- src/components/Navigation/Navigation_content.js
+++ src/components/Navigation/Navigation_content.js
@@ -1,20 +1,10 @@
 import React from "react";
 import { NAVIGATION_PANELS } from "../../config/const";
-function Links_panel() {
-    return <h1>links</h1>;
-}
-
-function Social_panel() {
-    return <h1>social</h1>;
-}
-
-function Statistics_panel() {
-    return <h1>stats</h1>;
-}
-
-function Feed_panel() {
-    return <h1>Feed</h1>;
-}
+import Links_panel from "./panels/Links_panel";
+import Social_panel from "./panels/Social_panel";
+import Statistics_panel from "./panels/Statistics_panel";
+import Feed_panel from "./panels/Feed_panel";
+import Alerts_panel from "./panels/Alerts_panel";
 
 function Alerts_panel() {
     return <h1>Alerts</h1>;
