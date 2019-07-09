333a2e6b6e4bdfc6bae39ea4a5860b85d6cfdf2c
bd2187 <brandon.dionisio.26@gmail.com>
2019-07-08 20:54:55 -0700

    Start using styled components

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import Navigation_header from "./Navigation_header";
 import Navigation_content from "./Navigation_content";
 import { NAVIGATION_PANELS } from "../../config/const";
+
+const Div = styled.div`
+    background-color: red;
+`;
 
 function Navigation() {
     const [panel, change_panel] = useState(NAVIGATION_PANELS.LINKS);
 
     return (
-        <div className="navigation">
+        <Div>
             <Navigation_header change_panel={change_panel} />
             <Navigation_content panel={panel} />
-        </div>
+        </Div>
     );
 }
 
