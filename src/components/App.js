8d4ddffa79fb98dfc72409f641a4d8593809a18b
bd2187 <brandon.dionisio.26@gmail.com>
2019-07-09 22:04:26 -0700

    Import App_context

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import routes from "../config/routes";
+import { App_context } from "../contexts/app-context";
 
 function App() {
-    return <>{routes}</>;
+    const [is_nav_open, toggle_nav] = React.useState(false);
+
+    const app_state = {
+        is_nav_open,
+        toggle_nav: () => toggle_nav(!is_nav_open)
+    };
+
+    return (
+        <App_context.Provider value={app_state}>{routes}</App_context.Provider>
+    );
 }
 
 export default App;
