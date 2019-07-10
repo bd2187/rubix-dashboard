4d7f040cbb087acc022516a66d3a4161f19158a3
bd2187 <brandon.dionisio.26@gmail.com>
2019-07-09 22:01:45 -0700

    Import App_context and make the Navigation component a consumer of the context. Style the Navigation_div.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { App_context } from "../../contexts/app-context";
 import Navigation_header from "./Navigation_header";
 import Navigation_content from "./Navigation_content";
 import { NAVIGATION_PANELS } from "../../config/const";
 
-const Div = styled.div`
+const Navigation_div = styled.div`
+    position: fixed;
+    top: 0;
+    left: ${({ is_nav_open }) => {
+        return is_nav_open ? "0" : "-200px";
+    }};
+    width: 200px;
     background-color: red;
+
+    transition: left 0.5s ease-in-out;
 `;
 
 function Navigation() {
     const [panel, change_panel] = useState(NAVIGATION_PANELS.LINKS);
-
     return (
-        <Div>
-            <Navigation_header change_panel={change_panel} />
-            <Navigation_content panel={panel} />
-        </Div>
+        <App_context.Consumer>
+            {function({ is_nav_open }) {
+                return (
+                    <Navigation_div is_nav_open={is_nav_open}>
+                        <Navigation_header change_panel={change_panel} />
+                        <Navigation_content panel={panel} />
+                    </Navigation_div>
+                );
+            }}
+        </App_context.Consumer>
     );
 }
 
