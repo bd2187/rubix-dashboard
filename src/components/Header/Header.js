cf0fc05a46551e7de3c5090f39db54f539d67ef1
bd2187 <brandon.dionisio.26@gmail.com>
2019-07-09 22:04:01 -0700

    Have the Header component consume the App_context. Style the Head component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { App_context } from "../../contexts/app-context";
+import Navigation from "../Navigation/Navigation";
 import styled from "styled-components";
 
 const Head = styled.div`
     background-color: blue;
+    position: fixed;
+    top: 0;
+    left: ${({ is_nav_open }) => (is_nav_open ? "200px" : "0")}
     height: 75px;
+    width: 100vw;
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: space-between;    
+    transition: left 0.5s ease-in-out;
 `;
 
 const Hamburger_menu = styled.div`
@@ -23,11 +30,20 @@ const Power = styled.div`
 
 function Header() {
     return (
-        <Head>
-            <Hamburger_menu />
-            <h1>rubix</h1>
-            <Power />
-        </Head>
+        <App_context.Consumer>
+            {function({ is_nav_open, toggle_nav }) {
+                return (
+                    <>
+                        <Head is_nav_open={is_nav_open}>
+                            <Hamburger_menu onClick={toggle_nav} />
+                            <h1>rubix</h1>
+                            <Power />
+                        </Head>
+                        <Navigation />
+                    </>
+                );
+            }}
+        </App_context.Consumer>
     );
 }
 
