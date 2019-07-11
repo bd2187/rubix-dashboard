639cb23dc995e598249a059b86dac8185be327e0
bd2187 <brandon.dionisio.26@gmail.com>
2019-07-10 22:05:25 -0700

    Initial commit. Declare reusable styles

---
+++ src/components/common/styled-components.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Nav_transition = styled.div`
+    transition: left 0.2s ease-in-out;
+`;
+
+export const flex_center_all = `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const pixels_to_move = "250px";
