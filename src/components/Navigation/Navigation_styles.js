a793f7e223dd939b6580fc0ae80ec886fe6003ee
bd2187 <brandon.dionisio.26@gmail.com>
2019-07-11 20:49:10 -0700

    Create the Navigation_panels_menu_ul styled component

--- src/components/Navigation/Navigation_styles.js
+++ src/components/Navigation/Navigation_styles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { Nav_transition, pixels_to_move } from "../common/styled-components";
 
+const nav_header_font_color = `rgba(0, 0, 0, 0.2)`;
+
 export const Navigation_div = styled(Nav_transition)`
     position: fixed;
     top: 0;
@@ -15,12 +17,10 @@ export const Navigation_div = styled(Nav_transition)`
 `;
 
 export const Navigation_header_div = styled.div`
-    background: #f25d50;
-
     .navigation-profile-container {
         color: #fff;
-        background: green;
-        padding: 25px 25px;
+        background: #fa7252;
+        padding: 17px 25px;
         display: flex;
         align-items: center;
         justify-content: space-between;
@@ -31,6 +31,10 @@ export const Navigation_header_div = styled.div`
         height: 40px;
         border-radius: 50%;
         background-color: magenta;
+    }
+
+    .navigation-profile-container__lock {
+        color: ${nav_header_font_color};
     }
 
     .profile-info {
@@ -44,7 +48,7 @@ export const Navigation_header_div = styled.div`
             }
 
             h1 {
-                font-size: 17px;
+                font-size: 15px;
                 margin: 3px 0;
             }
         }
@@ -52,13 +56,36 @@ export const Navigation_header_div = styled.div`
 
     .profile-progress-bar {
         width: 100%;
-        background: red;
-        height: 3px;
+        background: rgba(0, 0, 0, 0.2);
+        height: 4px;
     }
 
     .profile-progress-bar__progress {
         width: 25%;
         height: 100%;
         background: #fff;
+    }
+`;
+
+export const Navigation_panels_menu_ul = styled.ul`
+    display: flex;
+
+    .navigation-panel-option {
+        width: 20%;
+        background: #e36049;
+        text-align: center;
+        padding: 10px 0;
+        border-right: 0.5px solid ${nav_header_font_color};
+        color: ${nav_header_font_color};
+        cursor: pointer;
+        font-size: 14px;
+        :hover {
+            background: #cc5641;
+        }
+    }
+
+    .navigation-panel-option--selected,
+    .navigation-panel-option--selected:hover {
+        background-color: #b84d3b;
     }
 `;
