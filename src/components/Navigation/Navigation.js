b0c4cc13ddaf5ddb85216f94fd63476dfbac617e
bd2187 <brandon.dionisio.26@gmail.com>
2019-07-14 11:52:43 -0700

    Import Navigation_container_div

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { App_context } from "../../contexts/app-context";
 import Navigation_header from "./Navigation_header";
 import Navigation_content from "./Navigation_content";
-import { Navigation_div } from "./Navigation_styles";
+import { Navigation_container_div, Navigation_div } from "./Navigation_styles";
 import { NAVIGATION_PANELS } from "../../config/const";
 
 function Navigation() {
@@ -12,13 +12,15 @@ function Navigation() {
         <App_context.Consumer>
             {function({ is_nav_open }) {
                 return (
-                    <Navigation_div is_nav_open={is_nav_open}>
+                    <Navigation_container_div is_nav_open={is_nav_open}>
                         <Navigation_header
                             panel={panel}
                             change_panel={change_panel}
                         />
-                        <Navigation_content panel={panel} />
-                    </Navigation_div>
+                        <Navigation_div>
+                            <Navigation_content panel={panel} />
+                        </Navigation_div>
+                    </Navigation_container_div>
                 );
             }}
         </App_context.Consumer>
