68a0ecfd2fefa9e3651eb43b93bc0f1af643247a
bd2187 <brandon.dionisio.26@gmail.com>
2019-07-29 20:36:16 -0700

    Initial commit. Render Mailbox component

---
+++ src/components/Mailbox/Mailbox.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../common/Card/Card";
+
+function Mailbox() {
+    return (
+        <Card>
+            <h1>Mailbox</h1>
+        </Card>
+    );
+}
+
+export default Mailbox;
