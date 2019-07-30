8d923ddada932e67667032d809f80d0ea3b8e87c
bd2187 <brandon.dionisio.26@gmail.com>
2019-07-29 20:58:42 -0700

    add height and padding properties to wrapper_div styled component

--- src/components/Wrapper/Wrapper_styles.js
+++ src/components/Wrapper/Wrapper_styles.js
@@ -2,8 +2,10 @@ import styled from "styled-components";
 import { Nav_transition, pixels_to_move } from "../common/styled-components";
 
 export const Wrapper_div = styled(Nav_transition)`
-    background: orange;
+    background: blue;
     position: relative;
+    height: calc(100vh - 75px);
     left: ${({ is_nav_open }) => (is_nav_open ? pixels_to_move : "0")};
     margin-top: 75px;
+    padding: 35px;
 `;
